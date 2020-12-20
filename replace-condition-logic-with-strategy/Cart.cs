78e894ccc2a153e048fb9c37e9de2188b44cc510
Joey Chen <jay.hatelove@gmail.com>
2020-12-20 13:30:14 +0800

    init

---
+++ replace-condition-logic-with-strategy/Cart.cs
@@ -0,0 +1,49 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace replace_condition_logic_with_strategy
+{
+    public class Cart
+    {
+        public double shippingFee(string shipper, double length, double width, double height, double weight)
+        {
+            if (shipper.Equals("black cat"))
+            {
+                if (weight > 20)
+                {
+                    return 500;
+                }
+                else
+                {
+                    return 100 + weight * 10;
+                }
+            }
+            else if (shipper.Equals("hsinchu"))
+            {
+                double size = length * width * height;
+                if (length > 100 || width > 100 || height > 100)
+                {
+                    return size * 0.00002 * 1100 + 500;
+                }
+                else
+                {
+                    return size * 0.00002 * 1200;
+                }
+            }
+            else if (shipper.Equals("post office"))
+            {
+                double feeByWeight = 80 + weight * 10;
+                double size = length * width * height;
+                double feeBySize = size * 0.00002 * 1100;
+                return feeByWeight < feeBySize ? feeByWeight : feeBySize;
+            }
+            else
+            {
+                throw new ArgumentException("shipper not exist");
+            }
+        }
+    }
+}
