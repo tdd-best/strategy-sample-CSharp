fa491888cd7f33d88e8c16ac5b43852809c275de
Joey Chen <jay.hatelove@gmail.com>
2020-12-20 16:36:27 +0800

    refactor: use product.Size() and inline variables

--- replace-condition-logic-with-strategy/Cart.cs
+++ replace-condition-logic-with-strategy/Cart.cs
@@ -38,8 +38,7 @@ namespace replace_condition_logic_with_strategy
             else if (shipper.Equals("post office"))
             {
                 var feeByWeight = 80 + product.Weight * 10;
-                var size = product.Length * product.Width * product.Height;
-                var feeBySize = size * 0.00002 * 1100;
+                var feeBySize = product.Size() * 0.00002 * 1100;
                 return Math.Min(feeByWeight, feeBySize);
             }
             else
